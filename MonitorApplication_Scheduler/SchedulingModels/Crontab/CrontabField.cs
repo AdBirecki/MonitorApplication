08585408cfc67673cc35d3d40a132dfcbeacda87
Arnulf <abirecki@gmail.com>
2018-10-14 23:08:05 +0200

    refactoring pt 2

--- MonitorApplication_Scheduler/SchedulingModels/Crontab/CrontabField.cs
+++ MonitorApplication_Scheduler/SchedulingModels/Crontab/CrontabField.cs
@@ -88,8 +88,107 @@ namespace MonitorApplication_Models.Scheduling.Crontab
 
             if (start == end)
             {
+                if (start < 0)
+                {
+                    //
+                    // We're setting the entire range of values.
+                    //
 
+                    if (interval <= 1)
+                    {
+                        _minValueSet = minValue;
+                        _maxValueSet = maxValue;
+                        _bits.SetAll(true);
+                        return;
+                    }
+
+                    start = minValue;
+                    end = maxValue;
+                }
+                else
+                {
+                    //
+                    // We're only setting a single value - check that it is in range.
+                    //
+
+                    if (start < minValue)
+                    {
+                        throw new FormatException(string.Format(
+                            "'{0} is lower than the minimum allowable value for this field. Value must be between {1} and {2} (all inclusive).",
+                            start, _impl.MinValue, _impl.MaxValue));
+                    }
+
+                    if (start > maxValue)
+                    {
+                        throw new FormatException(string.Format(
+                            "'{0} is higher than the maximum allowable value for this field. Value must be between {1} and {2} (all inclusive).",
+                            end, _impl.MinValue, _impl.MaxValue));
+                    }
+                }
             }
+            else
+            {
+                //
+                // For ranges, if the start is bigger than the end value then
+                // swap them over.
+                //
+
+                if (start > end)
+                {
+                    end ^= start;
+                    start ^= end;
+                    end ^= start;
+                }
+
+                if (start < 0)
+                {
+                    start = minValue;
+                }
+                else if (start < minValue)
+                {
+                    throw new FormatException(string.Format(
+                        "'{0} is lower than the minimum allowable value for this field. Value must be between {1} and {2} (all inclusive).",
+                        start, _impl.MinValue, _impl.MaxValue));
+                }
+
+                if (end < 0)
+                {
+                    end = maxValue;
+                }
+                else if (end > maxValue)
+                {
+                    throw new FormatException(string.Format(
+                        "'{0} is higher than the maximum allowable value for this field. Value must be between {1} and {2} (all inclusive).",
+                        end, _impl.MinValue, _impl.MaxValue));
+                }
+            }
+
+            if (interval < 1)
+                interval = 1;
+
+            int i;
+
+            //
+            // Populate the _bits table by setting all the bits corresponding to
+            // the valid field values.
+            //
+
+            for (i = start - minValue; i <= (end - minValue); i += interval)
+                _bits[i] = true;
+
+            //
+            // Make sure we remember the minimum value set so far Keep track of
+            // the highest and lowest values that have been added to this field
+            // so far.
+            //
+
+            if (_minValueSet > start)
+                _minValueSet = start;
+
+            i += (minValue - interval);
+
+            if (_maxValueSet < i)
+                _maxValueSet = i;
         }
     }
 }
