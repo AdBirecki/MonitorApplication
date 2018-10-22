fbd8cc683943c34cbb1df01c73a9b43f46fccb61
Arnulf <abirecki@gmail.com>
2018-10-22 21:26:36 +0200

    EF pt 3

---
+++ MonitorApplication/temp/Utilities.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MonitorApplication.temp
+{
+    public static class Utilities
+    {
+        public static int staticField { get; set; }
+       
+    }
+}
---
+++ MonitorApplication_Models/UserModels/Message.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitorApplication_Models.UserModels
+{
+    public class Message
+    {
+        public int MessageId { get; set; }
+        public string Message { get; set; }
+
+    }
+}
