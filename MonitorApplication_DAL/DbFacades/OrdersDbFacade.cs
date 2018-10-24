86070679cf75345ad426a5de3669e9c32d427c86
bireckia <Birecki.A@eplan.pl>
2018-10-24 16:07:34 +0200

    EF pt 11

---
+++ MonitorApplication_DAL/DbFacades/OrdersDbFacade.cs
@@ -0,0 +1,40 @@
+﻿using MonitorApplication_Models.UserModels;
+using MonitorApplication_USERS_DAL.Contexts;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace MonitorApplication_USERS_DAL.DbFacades
+{
+    public class OrdersDbFacade : IDisposable
+    {
+        private readonly OrdersContext _orderContext;
+        private readonly Autofac.IContainer _container;
+        public OrdersDbFacade(OrdersContext ordersContext, Autofac.IContainer container)
+        {
+            _orderContext = ordersContext;
+            _container = container;
+        }
+
+        public IQueryable<User> User {
+            get { return _orderContext.Users; }
+        }
+
+        public IQueryable<UserOrders> UserOrders {
+            get { return _orderContext.UserOrders;}
+        }
+
+        public IQueryable<PurchaseOrder> PurchaseOrders {
+            get { return _orderContext.PurchaseOrders; }
+        }
+
+
+
+        public void Dispose()
+        {
+            _orderContext.Dispose();
+        }
+    }
+}
---
+++ MonitorApplication_DAL/Module/DbModules.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using MonitorApplication_USERS_DAL.DbFacades;
+using MonitorApplication_USERS_DAL.Interfaces;
+
+namespace MonitorApplication_USERS_DAL.Module
+{
+    public class DbModules: Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<OrdersDbFacade>()
+                .As<IOrdersDbFacade>()
+                .InstancePerDependency();
+        }
+    }
+}
