10fcb8013b70de7efadedfb09a3c5d5f69be2fd1
bireckia <Birecki.A@eplan.pl>
2018-10-25 15:55:10 +0200

    File upload

---
+++ MonitorApplication/Controllers/FileController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MonitorApplication.Controllers.BaseControllers;
+using MonitorApplication_BL.Commands.Interfaces;
+using MonitorApplication_BL.Commands.RegisterCommand;
+using MonitorApplication_BL.Queries.Interfaces;
+
+namespace MonitorApplication.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class FileController : ApiBaseController
+    {
+        private readonly ICommandDispatcher _commandDispatcher;
+        private readonly IQueryDispatcher _queryDispatcher;
+        public FileController(ICommandDispatcher commandDispatcher, IQueryDispatcher queryDisaptcher)
+        {
+            _commandDispatcher = commandDispatcher;
+            _queryDispatcher = queryDisaptcher;
+        }
+
+        [HttpPost]
+        public IActionResult PostFile([FromBody] SaveFileCommand command)
+        {
+            _commandDispatcher.Execute(command);
+            return Ok("Ok");
+        }
+    }
+}
---
+++ MonitorApplication_BL/Commands/RegisterCommand/SaveFileCommand.cs
@@ -0,0 +1,12 @@
+﻿using MonitorApplication_BL.Commands.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitorApplication_BL.Commands.RegisterCommand
+{
+    public class SaveFileCommand : ICommand, IFormFile
+    {
+
+    }
+}
