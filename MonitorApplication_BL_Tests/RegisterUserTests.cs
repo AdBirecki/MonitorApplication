147a3532e1f0064244c0814940d0db6a3932d2fe
Arnulf <abirecki@gmail.com>
2018-11-27 21:52:16 +0100

    Added unit tests stubs

---
+++ MonitorApplication_BL_Tests/RegisterUserTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace MonitorApplication_BL_Tests
+{
+    [TestFixture]
+    public class RegisterUserTests
+    {
+        [SetUp]
+        public void SpecSetUp()
+        {
+            Console.WriteLine("setUp is run here --> the test runs here [foo]");
+        }
+
+        [Test]
+        public void AreEqual()
+        {
+            Console.WriteLine("--> the test runs here [foo]");
+            Assert.AreEqual(1, 1);
+        }
+
+    }
+}
---
+++ MonitorApplication_BL_Tests/SaveMineralDataTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MonitorApplication_BL_Tests
+{
+    class SaveMineralDataTests
+    {
+    }
+}
