2fc65320c44941232660886b6585d33e44fa75b2
bireckia <Birecki.A@eplan.pl>
2018-11-28 13:29:01 +0100

    added tests

--- MonitorApplication_BL_Tests/RegisterUserTests.cs
+++ MonitorApplication_BL_Tests/RegisterUserTests.cs
@@ -16,49 +16,42 @@ namespace MonitorApplication_BL_Tests
     public class RegisterUserTests
     {
         ILoggerFactory _loggerFactory;
+        OrdersDbContext _ordersDbContext;
+        User expectedUser_1;
+        User expectedUser_2;
+        User expectedUser_3;
+
         [SetUp]
         public void SpecSetUp()
         {
             _loggerFactory = new LoggerFactory();
 
-        }
-
-        [Test]
-        public void RegisterUser()
-        {
             #region Users mock data
-            User expectedUser_1 = new User() {
+            expectedUser_1 = new User()
+            {
                 Username = "TestUser1",
                 Password = "TestPassword1",
                 Name = "A",
                 Surname = "B"
             };
-            User expectedUser_2 = new User()
+            expectedUser_2 = new User()
             {
                 Username = "TestUser2",
                 Password = "TestPassword2",
                 Name = "A",
                 Surname = "B"
             };
-            User expectedUser_3 = new User()
+            expectedUser_3 = new User()
             {
                 Username = "TestUser3",
                 Password = "TestPassword3",
                 Name = "A",
                 Surname = "B"
             };
-            #endregion
 
-            RegisterUserCommand registerUserCommand = new RegisterUserCommand()
-            {
-                UserName = "TestUser1",
-                Password = "TestPassword",
-                Name = "A",
-                Surname = "B"
-            };
             // DbSet
             var mockUsersData = new User[] { expectedUser_1, expectedUser_2, expectedUser_3 }.AsQueryable();
-            DbSet<User> mockSet = Substitute.For<DbSet<User>,IQueryable<User>>();
+            DbSet<User> mockSet = Substitute.For<DbSet<User>, IQueryable<User>>();
 
             // create mock for DbSet
             ((IQueryable<User>)mockSet).Provider.Returns(mockUsersData.Provider);
@@ -66,10 +59,55 @@ namespace MonitorApplication_BL_Tests
             ((IQueryable<User>)mockSet).ElementType.Returns(mockUsersData.ElementType);
             ((IQueryable<User>)mockSet).GetEnumerator().Returns(mockUsersData.GetEnumerator());
 
-            var mockOrdersDbContex = Substitute.For<OrdersDbContext>();
-            mockOrdersDbContex.Users.Returns(mockSet);
+            _ordersDbContext = Substitute.For<OrdersDbContext>();
+            _ordersDbContext.Users.Returns(mockSet);
+            #endregion
 
-            RegisterUserHandler registerUserHandler = new RegisterUserHandler(_loggerFactory, mockOrdersDbContex);
+        }
+
+        [Test]
+        public void RegisterInvalidPassword()
+        {
+          RegisterUserCommand registerUserCommand = new RegisterUserCommand()
+          {
+                UserName = "TestUser4",
+                Password = "",
+                Name = "Name",
+                Surname = "Surname"
+          };
+
+           bool isValid = registerUserCommand.IsValid();
+
+           Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void RegisterInvalidUserName()
+        {
+            RegisterUserCommand registerUserCommand = new RegisterUserCommand()
+            {
+                UserName = "",
+                Password = "",
+                Name = "Name",
+                Surname = "Surname"
+            };
+
+            bool isValid = registerUserCommand.IsValid();
+            Assert.IsFalse(isValid);
+        }
+
+        [Test]
+        public void RegisterUserException()
+        {
+            RegisterUserCommand registerUserCommand = new RegisterUserCommand()
+            {
+                UserName = "TestUser1",
+                Password = "TestPassword",
+                Name = "A",
+                Surname = "B"
+            };
+
+            RegisterUserHandler registerUserHandler = new RegisterUserHandler(_loggerFactory, _ordersDbContext);
             Assert.Throws<ArgumentException>(() => { registerUserHandler.Execute(registerUserCommand);});
         }
 
